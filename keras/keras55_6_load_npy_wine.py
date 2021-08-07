cf253d71af7793ee6c329f7926ca986377253052
variablejun <crescendo0217@gmain.com>
2021-08-07 23:58:07 +0900

    first commit

---
+++ keras/keras55_6_load_npy_wine.py
@@ -0,0 +1,7 @@
+import numpy as np
+x_data = np.load('./_save/_npy/k55_x_data_wine.npy')
+y_data = np.load('./_save/_npy/k55_y_data_wine.npy')
+print(x_data)
+print(y_data)
+
+print(x_data.shape,y_data.shape) #(178, 13) (178,)
